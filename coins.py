f054a95262cb93947dcd8d1e37221218df6306a4
PlayfulMathematician <73261021+PlayfulMathematician@users.noreply.github.com>
2022-07-16 09:34:52 -0600

    Update coins.py

--- coins.py
+++ coins.py
@@ -2,7 +2,12 @@ def nFlip(x,n,k):
     r=list(map((lambda t: 1),list(range(0,x)))
     if k+n>x:
         r[k:n+k]=list(map((lambda x:1-x),r[k:n+k]))
-        return k
+        return r
     else:
         raise ArithmeticError()
          
+def binList(x):
+    if x==1:
+        return [[0],[1]]
+    else:
+        return list(map(lambda y:[0]+y,binList(x-1)))+list(map(lambda y:[1]+y,binList(x-1)))
