7a184c37d9b01c247db6ac1c130dc56b4d1c2517
PlayfulMathematician <73261021+PlayfulMathematician@users.noreply.github.com>
2022-07-16 09:31:37 -0600

    Update coins.py

--- coins.py
+++ coins.py
@@ -1,1 +1,8 @@
-
+def nFlip(x,n,k):
+    r=list(map((lambda t: 1),list(range(0,x)))
+    if k+n>x:
+        r[k:n+k]=list(map((lambda x:1-x),r[k:n+k]))
+        return k
+    else:
+        raise ArithmeticError()
+         
