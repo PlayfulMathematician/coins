2e60a0fc36bf764a325ef873605c350aac6fc5c2
PlayfulMathematician <73261021+PlayfulMathematician@users.noreply.github.com>
2022-07-16 09:50:30 -0600

    Update coins.py

--- coins.py
+++ coins.py
@@ -1,5 +1,5 @@
-def nFlip(x,n,k):
-    r=list(map((lambda t: 1),list(range(0,x)))
+def nFlip(r,n,k):
+    x=len(r)
     if k+n>x:
         r[k:n+k]=list(map((lambda x:1-x),r[k:n+k]))
         return r
@@ -11,3 +11,13 @@ def binList(x):
         return [[0],[1]]
     else:
         return list(map(lambda y:[0]+y,binList(x-1)))+list(map(lambda y:[1]+y,binList(x-1)))
+def sequenceOfNFlip(x,n,bin_list):
+    r=list(map((lambda t: 1),list(range(0,x))))
+    for i,q in enumerate(bin_list):
+        if q==0:
+            continue
+        else:
+            r=nFlip(r,n,i)
+    return r
+            
+    
